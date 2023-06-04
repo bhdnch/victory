8a13459fcfb1457b3b77621b43b2e9ae7abd1b44
Bohdan Bilous <bohdanbilous.l@gmail.com>
2023-06-04 12:11:31 +0200

    bug fixed

--- general/js/burger.js
+++ general/js/burger.js
@@ -1,3 +1,4 @@
+const html = document.querySelector("html");
 const burgerWrapper = document.querySelector(".intro__burger__wrapper");
 const burger = document.querySelector(".intro__burger");
 const burgerMenu = document.querySelector(".intro__burger-menu");
@@ -5,4 +6,5 @@ const burgerMenu = document.querySelector(".intro__burger-menu");
 burgerWrapper.onclick = () => {
     burger.classList.toggle("menu-on");
     burgerMenu.classList.toggle("intro__burger-menu_open");
+    html.classList.toggle("stop-scroll");
 };
