f18a999aa32adcf16f2b3c9d67dc51829f270671
Bohdan Bilous <bohdanbilous.l@gmail.com>
2023-06-04 18:04:36 +0200

    code optimized

--- general/js/navigationAnim.js
+++ general/js/navigationAnim.js
@@ -6,16 +6,19 @@ const hr = document.querySelector(".intro__hr");
 const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
 const containHide = () => header.classList.contains("intro__nav-small");
 
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 0 && !containHide()) {
+window.addEventListener("scroll", handleScroll);
+
+function handleScroll() {
+    const currentScroll = scrollPosition();
+
+    if (currentScroll > 0 && !containHide()) {
         //scroll down
         header.classList.add("intro__nav-small");
         hr.classList.add("hideHR");
-    } else if (window.pageYOffset < 1 && containHide()) {
+    } else if (currentScroll < 1 && containHide()) {
         //scroll up
         header.classList.remove("intro__nav-small");
         hr.classList.remove("hideHR");
     }
-
-    lastScroll = scrollPosition();
-});
+    lastScroll = currentScroll;
+}
